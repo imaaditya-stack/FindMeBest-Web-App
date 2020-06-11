09084e902cb5e695a100ff9f57d608c7df89243a
Aaditya Ghag <59827782+imaaditya-stack@users.noreply.github.com>
2020-06-11 16:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, flash, request, redirect, url_for, render_template
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--remote-debugging-port=9222')
-
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -35,26 +27,22 @@ def price():
 
         search = request.form['inputsearch']
 
-#         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
         # Amazon
 
-#         amazonresponse = requests.get("https://www.amazon.in/s?k="+search.lower(),headers=headers)
+        amazonresponse = requests.get("https://www.amazon.in/s?k="+search.lower(),headers=headers)
 
-#         amazoncontent = amazonresponse.content
-        
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-        driver.get("https://www.amazon.in/s?k="+search.lower())
+        amazoncontent = amazonresponse.content
 
         #BeautifulSoup Object Initialization
-        amazonsoupobject = BeautifulSoup(driver.page_source,"html.parser")
+        amazonsoupobject = BeautifulSoup(amazoncontent,"html.parser")
 
         amazonproducts = amazonsoupobject.find_all("div",attrs={"class": "s-include-content-margin"})
 
-#         if len(amazonproducts) < 1:
-#             flash("Please enter a valid product","danger")
-#             return redirect(url_for('home'))
+        if len(amazonproducts) < 1:
+            flash("Please enter a valid product","danger")
+            return redirect(url_for('home'))
 
         amazondata = []
 
@@ -66,11 +54,9 @@ def price():
                 #Try block store the data
                 #If exception occurs it will store empty string
 
-#                 if search.lower() in amazonproducts[product].find("img",attrs={"class":"s-image"})['alt'].lower():
+                if search.lower() in amazonproducts[product].find("img",attrs={"class":"s-image"})['alt'].lower():
 
                     #Restuarant Name
-                
-                if amazonproducts:
                     try:
                         amazondatadict['productname']=amazonproducts[product].find("img",attrs={"class":"s-image"})['alt']
                     except:
@@ -86,11 +72,6 @@ def price():
                     except:
                         amazondatadict['productimg']=''
 
-                    try:
-                        amazondatadict['productrating']=amazonproducts[product].find("div",attrs={"class":"hGSR34"}).text.strip()
-                    except:
-                        amazondatadict['productrating']=''
-
                     #Votes
                     try:
                         amazondatadict['productprice']=amazonproducts[product].find("a",attrs={"class":"a-size-base"}).next_element.next_element.find("span",attrs={"class":"a-offscreen"}).text.strip()
@@ -102,18 +83,23 @@ def price():
 
         # Flipkart
 
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        flipkartresponse = requests.get("https://www.flipkart.com/search?q="+search.lower(),headers=headers)
 
-        driver.get("https://www.flipkart.com/search?q="+search)
+        flipkartcontent = flipkartresponse.content
+
+
+        # driver=webdriver.Chrome(executable_path="chromedriver.exe")
+
+        # driver.get("https://www.flipkart.com/search?q="+search)
 
         #BeautifulSoup Object Initialization
-        flipkartsoupobject = BeautifulSoup(driver.page_source,"html.parser")
+        flipkartsoupobject = BeautifulSoup(flipkartcontent,"html.parser")
 
         flipkartproducts = flipkartsoupobject.find_all("div",attrs={"class": "_1UoZlX"})
 
-#         if len(flipkartproducts) < 1:
-#             flash("Please enter a valid product","danger")
-#             return redirect(url_for('home'))
+        if len(flipkartproducts) < 1:
+            flash("Please enter a valid product","danger")
+            return redirect(url_for('home'))
 
         flipkartdata = []
 
@@ -126,27 +112,28 @@ def price():
                 #If exception occurs it will store empty
                 if flipkartproducts:
 
-#                     if search.lower() in flipkartproducts[product].find("div",attrs={"class":"_3wU53n"}).text.strip().lower():
+                    if search.lower() in flipkartproducts[product].find("div",attrs={"class":"_3wU53n"}).text.strip().lower():
 
-                    try:
-                        flipkartdatadict['productname']=flipkartproducts[product].find("div",attrs={"class":"_3wU53n"}).text.strip()
-                    except:
-                        flipkartdatadict['productname']=''
+                        try:
+                            flipkartdatadict['productname']=flipkartproducts[product].find("div",attrs={"class":"_3wU53n"}).text.strip()
+                        except:
+                            flipkartdatadict['productname']=''
 
-                    try:
-                        flipkartdatadict['producturl']=flipkartproducts[product].find("a",attrs={"class":"_31qSD5"},href=True)['href']
-                    except:
-                        flipkartdatadict['producturl']=''
+                        try:
+                            flipkartdatadict['producturl']=flipkartproducts[product].find("a",attrs={"class":"_31qSD5"},href=True)['href']
+                        except:
+                            flipkartdatadict['producturl']=''
 
-                    try:
-                        flipkartdatadict['productimg']=flipkartproducts[product].find("img",attrs={"class":"_1Nyybr"})['src']
-                    except:
-                        flipkartdatadict['productimg']=''
+                        try:
+                            image = flipkartproducts[product].find_all("img")[0]
+                            flipkartdatadict['productimg']=image.get("src")
+                        except:
+                            flipkartdatadict['productimg']=''
 
-                    try:
-                        flipkartdatadict['productprice']=flipkartproducts[product].find("div",attrs={"class":"_1vC4OE"}).text.strip()
-                    except:
-                        flipkartdatadict['productprice']=''
+                        try:
+                            flipkartdatadict['productprice']=flipkartproducts[product].find("div",attrs={"class":"_1vC4OE"}).text.strip()
+                        except:
+                            flipkartdatadict['productprice']=''
 
                 flipkartdata.append(flipkartdatadict)
 
@@ -154,21 +141,18 @@ def price():
         #Snapdeal
 
 
-#         snapdealresponse = requests.get("https://www.snapdeal.com/search?keyword="+search,headers=headers)
+        snapdealresponse = requests.get("https://www.snapdeal.com/search?keyword="+search,headers=headers)
 
-#         snapdealcontent = snapdealresponse.content
-        
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        snapdealcontent = snapdealresponse.content
 
-        driver.get("https://www.snapdeal.com/search?keyword="+search.lower())
         #BeautifulSoup Object Initialization
-        snapdealsoupobject = BeautifulSoup(driver.page_source,"html.parser")
+        snapdealsoupobject = BeautifulSoup(snapdealcontent,"html.parser")
 
         snapdealproducts = snapdealsoupobject.find_all("div",attrs={"class": "product-tuple-listing"})
 
-#         if len(snapdealproducts) < 1:
-#             flash("Please enter a valid product","danger")
-#             return redirect(url_for('home'))
+        if len(snapdealproducts) < 1:
+            flash("Please enter a valid product","danger")
+            return redirect(url_for('home'))
 
         snapdealdata = []
 
@@ -179,8 +163,8 @@ def price():
                 #Try except blocks
                 #Try block store the data
                 #If exception occurs it will store empty string
-                if snapdealproducts:
-#                 if search.lower() in snapdealproducts[product].find("p",attrs={"class":"product-title"}).text.strip().lower():
+
+                if search.lower() in snapdealproducts[product].find("p",attrs={"class":"product-title"}).text.strip().lower():
 
                     try:
                         snapdealdatadict['productname']=snapdealproducts[product].find("p",attrs={"class":"product-title"}).text.strip()
@@ -188,7 +172,7 @@ def price():
                         snapdealdatadict['productname']=''
 
                     try:
-                        snapdealdatadict['ProductPrice']=snapdealproducts[product].find("div",attrs={"class":"product-price-row"}).find("span",attrs={"class":"product-price"}).text.strip()
+                        snapdealdatadict['productprice']=snapdealproducts[product].find("div",attrs={"class":"product-price-row"}).find("span",attrs={"class":"product-price"}).text.strip()
                     except:
                         snapdealdatadict['productprice']=''
 
@@ -208,5 +192,4 @@ def price():
     return render_template('main.html',amazon=amazondata,flipkart=flipkartdata,snapdeal=snapdealdata,query=search)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run()
